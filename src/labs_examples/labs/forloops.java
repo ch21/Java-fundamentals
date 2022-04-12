e4aacfa74d6ac4f100b888085268ae062251b9e0
denis <ch86772@gmail.com>
2022-04-11 23:29:46 -0400

    added a for loop class

---
+++ src/labs_examples/labs/forloops.java
@@ -0,0 +1,29 @@
+package labs_examples.labs;
+
+public class forloops{
+
+    public static void main(String[]args) {
+
+        //please write and run at least 3 unique if statments below
+        int i = 10;
+
+        if (i < 20){
+            if (i < 19) {
+                if(i > 19){
+                    System.out.println("lets see if this will work");
+                }
+                System.out.println("i is less than 20!");
+            }
+            else  {
+                System.out.println("hello Im here ");
+            }
+            System.out.println("this one should apear on here ");
+        }
+
+    }
+}
+
+
+
+
+
