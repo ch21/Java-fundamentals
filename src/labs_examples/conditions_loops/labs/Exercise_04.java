07ceddf3874fb853dbd0606a623009c2f0ffbd7e
denis <ch86772@gmail.com>
2022-04-17 15:28:53 -0400

    cleaned up comments

--- src/labs_examples/conditions_loops/labs/Exercise_04.java
+++ src/labs_examples/conditions_loops/labs/Exercise_04.java
@@ -10,9 +10,7 @@ import java.util.Scanner;
 public class Exercise_04 {
 
     public static void main(String[] args) {
-//        Scanner evenNum = new Scanner(System.in);
-//        System.out.println("Enter a number from 1-100");
-//        int evenNumbers = evenNum.nextInt();
+//
 
         for(int even = 0; even <= 100; even = even + 2  ){
             System.out.println("the number is: " + even);
