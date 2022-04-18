c66147f3de95eb89b182e3c667d05188edcc5b5a
denis <ch86772@gmail.com>
2022-04-17 23:15:46 -0400

    just finished exercise 06 would like to talk about exercise 5

--- src/labs_examples/conditions_loops/labs/Exercise_06.java
+++ src/labs_examples/conditions_loops/labs/Exercise_06.java
@@ -8,4 +8,11 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_06 {
+     public static void main(String[] args) {
+         int number = 1;
+        while (number<=100){
+            System.out.println("the number is: " + number);
+            number++;
+        }
+    }
 }
