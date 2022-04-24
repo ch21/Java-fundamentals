33c18dc12a4e8a456d116315aa8f5edfb1aa1838
denis <ch86772@gmail.com>
2022-04-23 23:45:11 -0400

    woked on exercise_10

--- src/labs_examples/conditions_loops/labs/Exercise_10.java
+++ src/labs_examples/conditions_loops/labs/Exercise_10.java
@@ -8,4 +8,14 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_10 {
+    public static void main(String[] args) {
+
+        for(int i = 0; i<=10; i++) {
+            if(i==6){
+                continue; // iterate and should skip 6
+            }
+            System.out.println(i);
+        }
+
+    }
 }
